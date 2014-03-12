a5c0794b8a8aada0838153a24560dc8fb3c9fce9
Mikko Lakomaa <mikko.lakomaa@nordsoftware.com>
2014-03-12 16:07:30 +0200

    Clean up baseconfig

--- puppet/modules/baseconfig/manifests/init.pp
+++ puppet/modules/baseconfig/manifests/init.pp
@@ -8,28 +8,12 @@ class baseconfig {
     command => '/usr/bin/apt-get update'
   }
 
-  exec { 'apt-get update again':
-    command => '/usr/bin/apt-get update'
-  }
-
   # Run apt-get update before packages are loaded
   Exec["apt-get update"] -> Package <| |>
 
   package { 'python-software-properties':
     ensure => present
   }
-
-  exec { 'change PHP repo':
-    command => '/usr/bin/apt-add-repository ppa:ondrej/php5-oldstable',
-    onlyif => '/usr/bin/test -e /usr/bin/apt-add-repository'
-  }
-
-  exec { 'add latest NodeJS repo':
-    command => '/usr/bin/apt-add-repository ppa:chris-lea/node.js',
-    onlyif => '/usr/bin/test -e /usr/bin/apt-add-repository'
-  }
-
-  Package['python-software-properties'] -> Exec['change PHP repo'] -> Exec['add latest NodeJS repo'] ~> Exec['apt-get update again']
 
   host { 'hostmachine':
     ip => '192.168.0.1'
@@ -43,6 +27,7 @@ class baseconfig {
       source => 'puppet:///modules/baseconfig/bashrc'
   }
 
+  # Load additional packages
   package { 'vim':
       ensure => present
   }
